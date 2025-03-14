89d0223069a2305803f6164cf016fc02f6216e40
João_Filipe-Dev <joaofilipesoares1@gmail.com>
2025-03-14 12:55:13 -0300

    5 commit

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBagShopping, faChevronLeft, faChevronRight, faMagnifyingGlass, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faBagShopping, faCartShopping, faChevronLeft, faChevronRight, faMagnifyingGlass, faUser } from "@fortawesome/free-solid-svg-icons";
 
 import './index.css'
 import { useContext, useState } from "react";
@@ -14,7 +14,7 @@ const produtos = [
     {
         nome: 'Pc Gamer Fênix X50, NVIDIA GeForce RTX 4070',
         descricao: "Processador Intel Core i7-13700K, Placa de Vídeo NVIDIA GeForce RTX 4070, Memória RAM 32 GB DDR5, Armazenamento 1 TB SSD NVMe, Gabinete Corsair 4000D Airflow com 3 Fans RGB, Placa Mãe ASUS ROG Strix Z790-A Gaming WiFi, Fonte Corsair RM850x (2021)",
-        lista_img: ["pcgamer.jpg", "sample_0 (7).jpg"],
+        lista_img: ["/webshop/pcgamer.jpg", "/webshop/sample_0 (7).jpg"],
         valor: 8500,
     },
     {
@@ -22,35 +22,35 @@ const produtos = [
         nome: "Fone de Ouvido Gamer HyperX Cloud Stinger Core",
         descricao: "Experimente um som imersivo com o HyperX Cloud Stinger Core. Com drivers de 40mm, microfone com cancelamento de ruído e design leve, ele é perfeito para longas sessões de jogo.",
         valor: 299.90,
-        lista_img: ["fone.jpg"]
+        lista_img: ["/webshop/fone.jpg"]
     },
     {
         id: 2,
         nome: "Smart TV Samsung 55 polegadas 4K",
         descricao: "Desfrute de imagens impecáveis com a Smart TV Samsung 55 polegadas 4K. Com resolução 4 vezes maior que o Full HD, você terá cores vibrantes e detalhes impressionantes. Explore o mundo do streaming com o sistema Tizen e conecte-se com seus dispositivos favoritos via Bluetooth.",
         valor: 2999.00,
-        lista_img: ["sample_0 (10).jpg"]
+        lista_img: ["/webshop/sample_0 (10).jpg"]
     },
     {
         id: 3,
         nome: "Smartphone Xiaomi Redmi Note 11",
         descricao: "O Redmi Note 11 é a escolha perfeita para quem busca um smartphone com ótimo custo-benefício. Com tela AMOLED de 90Hz, câmera de 50MP e bateria de longa duração, ele oferece performance e design impecáveis.",
         valor: 1499.00,
-        lista_img: ["sample_0 (8).jpg"]
+        lista_img: ["/webshop/sample_0 (8).jpg"]
     },
     {
         id: 4,
         nome: "Câmera Digital Sony Alpha 6400",
         descricao: "Libere o fotógrafo que existe em você com a Sony Alpha 6400. Com sensor APS-C de 24.2MP, foco automático rápido e gravação de vídeo 4K, ela é ideal para capturar momentos incríveis com qualidade profissional.",
         valor: 4999.00,
-        lista_img: ["camera.jpg"]
+        lista_img: ["/webshop/camera.jpg"]
     },
     {
         id: 5,
         nome: "Tablet Samsung Galaxy Tab S8",
         descricao: "O Galaxy Tab S8 é a combinação perfeita de produtividade e entretenimento. Com tela AMOLED de 11 polegadas, S Pen integrada e processador potente, ele é ideal para trabalho, estudo e lazer.",
         valor: 5999.00,
-        lista_img: ["teclado.jpg"]
+        lista_img: ["/webshop/ipad.jpg"]
     }
 ]
 
@@ -94,8 +94,8 @@ export default function Home() {
     return (
         <div className="page home">
             <nav className="top-nav">
-                <a href="/">
-                    <span>eShop <p>loja online</p></span>
+                <a href="/webshop">
+                    <span>eShop <p>loja online <FontAwesomeIcon icon={faCartShopping} /></p></span>
                 </a>
                 <nav>
                     <button>Categorias</button>
@@ -126,7 +126,7 @@ export default function Home() {
                         </span>
                         <button>Ver Mais</button>
                     </div>
-                    <img src="smartwatch.jpeg" alt="" />
+                    <img src="/webshop/smartwatch.jpeg" alt="" />
                 </div>
 
                 <div className="highlights">
